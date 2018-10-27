eaefa0674d38bf14efde6e0d364116d5d520c2da
shangjiaxuan <34389517+shangjiaxuan@users.noreply.github.com>
2018-10-27 18:45:58 +0800

    added sorted output

--- NetworkPrediction/FileReader.h
+++ NetworkPrediction/FileReader.h
@@ -10,8 +10,10 @@ struct list {
 struct network_data {
 	list* map = nullptr;
 	int num_of_people = 0;
-	int* person_index = nullptr;
-
+	//array of the index within calculation (convert input index to true index)
+	int* index = nullptr;
+	//array of the index the outside world knows
+	int* people = nullptr;
 	list* operator[](int rol) const {
 		return (map + (rol * num_of_people));
 	}
@@ -20,4 +22,5 @@ struct network_data {
 class FileReader {
 public:
 	static network_data read_file(const std::string& file);
+	static void write_sorted(const std::string& file, const network_data& data);
 };
--- NetworkPrediction/main.cpp
+++ NetworkPrediction/main.cpp
@@ -5,11 +5,16 @@ using namespace std;
 
 int main() {
 	cout << "Reading file...\n" << endl;
-	const chrono::time_point<chrono::steady_clock> start = std::chrono::steady_clock::now();
-	FileReader::read_file("Link prediction task.URL.txt");
+	chrono::time_point<chrono::steady_clock> start = std::chrono::steady_clock::now();
+	network_data data = FileReader::read_file("Link prediction task.URL.txt");
 	chrono::duration<double> dur = std::chrono::steady_clock::now() - start;
 	cout << std::setprecision(9);
 	cout << "Time used reading file:\n" << dur.count() << " seconds.\n" << endl;
+	start = std::chrono::steady_clock::now();
+	FileReader::write_sorted("out.txt", data);
+	dur = std::chrono::steady_clock::now() - start;
+	cout << std::setprecision(9);
+	cout << "Time used writing file:\n" << dur.count() << " seconds.\n" << endl;
 	system("pause");
 	return 0;
 }
