69b989deacafc2ef1829bbea340d41f6ed8502a8
shangjiaxuan <34389517+shangjiaxuan@users.noreply.github.com>
2018-10-29 17:28:52 +0800

    Update framework

---
+++ NetworkPrediction/network_data.cpp
@@ -0,0 +1,110 @@
+#include "network_data.h"
+
+list::~list() {
+	destroy(*this);
+}
+
+list::list(const list& source) {
+	copy(source, *this);
+}
+
+list::list(list&& source) noexcept {
+	move(source, *this);
+}
+
+list& list::operator=(const list& source) {
+	copy(source, *this);
+	return *this;
+}
+
+list& list::operator=(list&& source) noexcept {
+	move(source, *this);
+	return *this;
+}
+
+void list::destroy(list& source) {
+	delete[] source.data;
+	source.data = nullptr;
+}
+
+void list::copy(const list& source, list& dest) {
+	dest.sum = source.sum;
+	dest.num = source.num;
+	if(dest.num) {
+		dest.data = new int[dest.num];
+		for (int i = 0; i < dest.num; i++) {
+			dest.data[i] = source.data[i];
+		}
+	}
+}
+
+void list::move(list& source, list& dest) noexcept {
+	dest.sum = source.sum;
+	dest.num = source.num;
+	dest.data = source.data;
+	source.data = nullptr;
+}
+
+network_data::~network_data() {
+	destroy(*this);
+}
+
+network_data::network_data(const network_data& source) {
+	copy(source, *this);
+}
+
+network_data::network_data(network_data&& source) noexcept {
+	move(source, *this);
+}
+
+network_data& network_data::operator=(const network_data& source) {
+	copy(source, *this);
+	return *this;
+}
+
+network_data& network_data::operator=(network_data&& source) noexcept {
+	move(source, *this);
+	return *this;
+}
+
+void network_data::destroy(network_data& source) {
+	delete[] source.index;
+	source.index = nullptr;
+	delete[] source.people;
+	source.people = nullptr;
+	delete[] source.map;
+	source.map = nullptr;
+}
+
+void network_data::copy(const network_data& source, network_data& dest) {
+	dest.max_index = source.max_index;
+	dest.index = new int[dest.max_index];
+	for(int i=0; i<dest.max_index; i++) {
+		dest.index[i] = source.index[i];
+	}
+	dest.num_of_people = source.num_of_people;
+	dest.people = new int[dest.num_of_people];
+	for (int i = 0; i < dest.num_of_people; i++) {
+		dest.people[i] = source.people[i];
+	}
+	const int map_size = dest.num_of_people*dest.num_of_people;
+	dest.map = new list[map_size];
+	for(int i=0; i<map_size; i++) {
+		dest.map[i] = source.map[i];
+	}
+}
+
+void network_data::move(network_data& source, network_data& dest) noexcept {
+	dest.num_of_people = source.num_of_people;
+	dest.max_index = source.max_index;
+	dest.people = source.people;
+	source.people = nullptr;
+	dest.index = source.index;
+	source.index = nullptr;
+	dest.map = source.map;
+	source.map = nullptr;
+}
+
+
+
+
---
+++ NetworkPrediction/network_data.h
@@ -0,0 +1,46 @@
+#pragma once
+
+#include "Header.h"
+
+struct list {
+	int num = 0;
+	uint64_t sum = 0;
+	int* data = nullptr;
+
+	list() = default;
+	~list();
+	list(const list& source);
+	list(list&& source) noexcept;
+	list& operator=(const list& source);
+	list& operator=(list&& source) noexcept;
+	static void move(list& source, list& dest) noexcept;
+	static void copy(const list& source, list& dest);
+	static void destroy(list& source);
+};
+
+struct network_data {
+	list* map = nullptr;
+	int num_of_people = 0;
+	int max_index = 0;
+	int num_of_directional_edge = 0;
+	int num_of_non_directional = 0;
+	//array of the index within calculation (convert input index to true index)
+	int* index = nullptr;
+	//array of the index the outside world knows
+	int* people = nullptr;
+
+	list* operator[](int rol) const {
+		return (map + (rol * num_of_people));
+	}
+
+	network_data() = default;
+	~network_data();
+	network_data(const network_data& source);
+	network_data(network_data&& source) noexcept;
+	network_data& operator=(const network_data& source);
+	network_data& operator=(network_data&& source) noexcept;
+	static void move(network_data& source, network_data& dest) noexcept;
+	static void copy(const network_data& source, network_data& dest);
+	static void destroy(network_data& source);
+};
+
