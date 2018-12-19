8a94bb71d6b744a30d14c292d490a580ba156ad0
shangjiaxuan <34389517+shangjiaxuan@users.noreply.github.com>
2018-12-19 18:54:08 +0800

    optimize mt

--- NetworkPrediction/ThreadManager.h
+++ NetworkPrediction/ThreadManager.h
@@ -9,8 +9,8 @@ public:
 	static std::vector<output> vector_async(std::vector<input>& source, output(*const function)(input&));
 	static std::vector<output> vector_async_copy(std::vector<input>& source, output(*const function)(input));
 private:
-	static void launch_function(char* lock, output(*const function)(input&), input* data, output* out);
-	static void launch_function_copy(char* lock, output(*const function)(input), input data, output* out);
+	static void launch_function(bool* lock, output(*const function)(input&), input* data, output* out);
+	static void launch_function_copy(bool* lock, output(*const function)(input), input data, output* out);
 };
 
 #include "ThreadManager.ipp"
